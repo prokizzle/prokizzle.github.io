fffca41bb00e89f91d9c745d764b4c23835c2162
Nick Prokesch <nick_prokesch@apple.com>
2020-05-05 23:03:35 -0500

    Create basic link page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Link, Button, Box, Flex } from "rebass/styled-components";
+import Gravatar from "react-gravatar";
+import { map } from "ramda";
+
+const links = [
+  { title: "LinkedIn", href: "http://linkedin.com/in/nickprokesch" },
+  { title: "GitHub", href: "https://github.com/prokizzle" },
+  {
+    title: "Professional CV/Resume",
+    href:
+      "https://prokizzle-cv.s3.amazonaws.com/Nick_Prokesch_VisualCV_Resume.pdf"
+  },
+  { title: "My Medium Articles", href: "https://medium.com/@nprokesch" }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Flex justifyContent="center" width="100vw" px={1}>
+      <Box justifyContet="center" width="100vw">
+        <Flex
+          width="100vw"
+          height="100vh"
+          alignItems="center"
+          flexDirection="column"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Box textAlign="center" alignItems="center">
+            <Box>
+              <h1>Nick Prokesch</h1>
+            </Box>
+            <Box>
+              <h2>Front-end Web Developer</h2>
+            </Box>
+          </Box>
+          <Box justifyContent="center">
+            <Gravatar email="nick@prokes.ch" size={200} />
+          </Box>
+          {map(
+            ({ title, href }) => (
+              <Box width={["100%", "400px"]} py={2}>
+                <Link href={href}>
+                  <Button width="100%" variant="primary">
+                    {title}
+                  </Button>
+                </Link>
+              </Box>
+            ),
+            links
+          )}
+        </Flex>
+      </Box>
+    </Flex>
   );
 }
 
