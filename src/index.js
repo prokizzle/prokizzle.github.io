146627a25869857d022935b421a6f4eaee75bb9a
Nick Prokesch <nick_prokesch@apple.com>
2020-05-05 22:26:47 -0500

    Start new react project

--- src/index.js
+++ src/index.js
@@ -1,34 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import "./index.scss";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import {
-  faBold,
-  faDownload,
-  faPaperclip,
-  faPhone,
-  faEnvelope,
-} from "@fortawesome/free-solid-svg-icons";
-import rootReducer from "./reducers/root.reducer";
-import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
-
-library.add(fab);
-library.add(faBold, faPaperclip, faDownload, faEnvelope, faPhone);
-
-const store = createStore(rootReducer);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
